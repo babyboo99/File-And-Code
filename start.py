1a969aad1a901a6b8f186ec69400ac2d679c1e12
babyboo99 <myduyentranthi2k4@gmail.com>
2025-06-26 19:59:15 +0700

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-
-from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Xin chào! Tôi đã sẵn sàng.")
-
-start_command = CommandHandler("start", start)
